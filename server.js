ec7a5135466e1192135ef2007f16db8594f38311
harshpreetsolutions1313 <harshpreetsolutions1313@gmail.com>
2025-12-11 17:39:07 +0530

    excluded ports defined

--- server.js
+++ server.js
@@ -79,7 +79,7 @@ app.use((err, req, res, next) => {
 });
 
 // Export for Vercel
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 module.exports = app;
