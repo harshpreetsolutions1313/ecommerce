bd1cbb91ec880c916bc3aff7d12199ba355eac5f
harshpreetsolutions1313 <harshpreetsolutions1313@gmail.com>
2025-12-24 16:28:24 +0530

    purchase works

--- routes/order.js
+++ routes/order.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { restrict } = require('../middleware/auth');
-const { createOrder, payForOrder } = require('../controllers/order');
+const { createOrder, payForOrder, createBatchOrder} = require('../controllers/order');
 
 router.post('/create', restrict, createOrder);
-
+router.post('/create-batch', restrict, createBatchOrder);
 router.post('/pay', payForOrder);
 
 module.exports = router;
